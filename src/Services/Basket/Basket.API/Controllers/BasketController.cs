db0e4d529b91970e5482eeacb4a9188d6b563c01
Hiquepuff <hiqzada@gmail.com>
2022-05-23 15:23:27 -0300

    Cache controller routes

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,41 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketCache;
+
+        public BasketController(IBasketRepository basketCache) => _basketCache = basketCache;
+
+        [HttpGet("{username}")]
+        [ProducesResponseType( typeof(BasketModel), (int)HttpStatusCode.OK )]
+        public async Task<ActionResult<BasketModel>> GetBasket(string username)
+        {
+            var basket = await _basketCache.GetBasket(username);
+
+            return Ok(basket ?? new BasketModel(username));
+        }
+
+        [HttpPost]
+        [ProducesResponseType( typeof(BasketModel), (int)HttpStatusCode.OK )]
+        public async Task<ActionResult<BasketModel>> UpdateBasket([FromBody] BasketModel basket) =>
+            Ok(await _basketCache.UpdateBasket(basket));
+
+        [HttpDelete]
+        [ProducesResponseType( (int)HttpStatusCode.OK )]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            await _basketCache.DeleteBasket(username);
+
+            return Ok();
+        }
+    }
+}
